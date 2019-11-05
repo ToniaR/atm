a85486be384cf5ac2dd3e529aa11cd5a6a8666fc
Tonia <antosia535@gmail.com>
2019-11-05 21:09:57 +0100

    add calculate amount with available notes process

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
       <h1>Withdraw cash easily!</h1>
       <div class="form">
-        <input type="text" id="amount" name="amount">
+        <input type="text" id="amount" name="amount" v-model="entryAmount">
         <div class="form__checkboxes">
           <span class="form-field">
             <label for="hungreed">100</label>
@@ -28,15 +28,99 @@
           </span>
         </div>
       </div>
+      <button @click="getNotes">Withdraw</button>
 
       <h2>Output</h2>
-      <p></p>
+      <p>{{ result }}</p>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      entryAmount: null,
+      notes: [100, 50, 20, 10],
+      choosenNotes: [],
+      result: []
+    }
+  },
+  watch: {
+    // entryAmount: function(value) {
+    //   value !== null ? this.getResult(value) : console.log('wprowadź wartość');
+    // } 
+  },
+  methods: {
+    getResult(amount) {
+      if(!amount) {
+        this.result = 'Empty set';
+      }
+      else {
+        this.calculate(amount);
+      }
+    },
+    getNotes() {
+      this.result = [];
+      if(this.entryAmount % 10 === 0) {
+        if(this.choosenNotes) {
+          this.calculate(this.entryAmount, this.choosenNotes);          
+        }
+        else {
+          this.calculate(this.entryAmount, this.notes);
+        }
+      }
+      else {
+        this.result.push('NoteUnavailableException');
+      }
+    },
+    calculate(amount, arr) {
+      let remainder = null, count = null; 
+      this.result = [];
+      if(amount > 0 ) {
+        console.log('r', this.result)
+        console.log(1, amount);
+        for(let i = 0; i < arr.length; i++) {
+          if(amount >= arr[i]) {
+            console.log(2 + ' arr[i]', arr[i]);
+            // this.result= this.amount % arr[i]; // 
+            let division = amount / arr[i];
+            console.log(3 + ' division', division);
 
+            count = Math.floor(division); // how much note we should add
+            console.log(4 + ' count', count);
+            remainder = amount % arr[i]; // the remainder from the division
+            console.log(5 + ' reminder', remainder);
+
+            this.addNotes(arr[i], count); // add notes to result
+            amount = remainder;
+            console.log(6 + ' amount', amount);
+          }
+          else {
+            console.log('GO ON!');
+          }
+        }
+      }
+
+      console.log(7 + ' result ', this.result);
+    },
+    addNotes(note, noteNum) {
+      if(noteNum > 1) {
+        for(let i = 0; i < noteNum; i++) {
+          this.result.push(note);
+        }
+      }
+      else {
+        this.result.push(note);
+      }
+
+    },
+    clearResult() {
+      let input = document.getElementById('amount');
+      input.addEventListener('oninput', () => {
+        this.result = [];
+      })
+    }
+  }
 }
 </script>
 
